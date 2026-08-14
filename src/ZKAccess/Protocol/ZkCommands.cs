d98910813726b3559c1b2d3d6f19cd5963a56c4a
Vahid Rassouli <vrassouli@gmail.com>
2026-08-14 13:49:15 +0330

    Add device info protocol commands

--- src/ZKAccess/Protocol/ZkCommands.cs
+++ src/ZKAccess/Protocol/ZkCommands.cs
@@ -2,11 +2,15 @@ namespace ZKAccess.Protocol;
 
 internal static class ZkCommands
 {
+    public const ushort OptionsRead = 11;
+    public const ushort GetVersion = 1100;
+
     public const ushort Connect = 1000;
     public const ushort Exit = 1001;
     public const ushort Auth = 1102;
 
     public const ushort AckOk = 2000;
     public const ushort AckError = 2001;
+    public const ushort AckData = 2002;
     public const ushort AckUnauthenticated = 2005;
 }
